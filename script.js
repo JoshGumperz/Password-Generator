66fcb605c74f9e1fc60192ac4bb2ecef1e248f27
Josh Gumperz <joshg3023@gmail.com>
2021-06-22 22:12:59 -0700

    added alerts to prompt user to enter their desired password length, if they would like their password to include lowercase letters, upercase letters, etc.

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", ":", ";", "[", "]", "{", "}", "/", "-", "_"]
+var passwordLength
+var includeLower
+var includeUpper
+var includeNums
+var includeSymbols
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  length();
+  lower();
+  upper();
+  includeNumbers();
+  specialChars();
+
+
+
+  var password = generatePassword(passwordLength, includeLower, includeUpper, includeNums, includeSymbols);
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +29,38 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function length(){
+  passwordLength = prompt("How many characters do you want your password to be? Minimum length: 8 characters. Maximum length: 128 characters.")
+      if(passwordLength < 8) {
+        alert("Password must be at least 8 characters")
+        length();
+      }
+      else if(passwordLength > 128) {
+        alert("Password cannot be more than 128 characters")
+        length();
+      }
+      else if(passwordLength != null) {
+        return passwordLength;
+      }
+}
+
+function lower(){
+  includeLower = confirm("Click Ok to include lower case letters in your password.")
+  return includeLower;
+}
+
+function upper(){
+  includeUpper = confirm("Click Ok to include upper case letters in your password.")
+  return includeUpper;
+}
+
+function includeNumbers(){
+  includeNums = confirm("Click Ok to include numbers in your password.")
+  return includeNums;
+}
+
+function specialChars(){
+  includeSymbols = confirm("Click Ok to include symbols/special characters letters in your password.")
+  return includeSymbols;
+}
