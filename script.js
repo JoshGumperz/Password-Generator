b53c4df14065cf14367c2e7241d224266cba3900
Josh Gumperz <joshg3023@gmail.com>
2021-06-23 20:48:05 -0700

    cleaned up/rearranged prompt code

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// index values 0-25 are lower case letters, 26-51 upper case letters, 52-60 numbers, and 61-80 are symbols
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", ":", ";", "[", "]", "{", "}", "/", "-", "_"]
+var passwordCharacters
 var passwordLength
 var includeLower
 var includeUpper
 var includeNums
 var includeSymbols
+
 // Write password to the #password input
 function writePassword() {
-  length();
-  lower();
-  upper();
-  includeNumbers();
-  specialChars();
+  userLengthChoice();
+  userChoices();
 
 
 
-  var password = generatePassword(passwordLength, includeLower, includeUpper, includeNums, includeSymbols);
+  var password = generatePassword();
   
   var passwordText = document.querySelector("#password");
 
@@ -30,37 +30,38 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function length(){
+function userLengthChoice(){
   passwordLength = prompt("How many characters do you want your password to be? Minimum length: 8 characters. Maximum length: 128 characters.")
-      if(passwordLength < 8) {
+      if (!passwordLength) {
+        return
+      }
+      else if(passwordLength < 8) {
         alert("Password must be at least 8 characters")
-        length();
+        userLengthChoice();
       }
       else if(passwordLength > 128) {
         alert("Password cannot be more than 128 characters")
-        length();
+        userLengthChoice();
       }
-      else if(passwordLength != null) {
+      else {
         return passwordLength;
       }
 }
-
-function lower(){
+function userChoices(){
   includeLower = confirm("Click Ok to include lower case letters in your password.")
-  return includeLower;
-}
 
-function upper(){
   includeUpper = confirm("Click Ok to include upper case letters in your password.")
-  return includeUpper;
-}
 
-function includeNumbers(){
   includeNums = confirm("Click Ok to include numbers in your password.")
-  return includeNums;
-}
 
-function specialChars(){
   includeSymbols = confirm("Click Ok to include symbols/special characters letters in your password.")
-  return includeSymbols;
+
+  if (!includeLower && !includeUpper && !includeNums && !includeSymbols) {
+    alert("Please choose at least one character type to include in your password.")
+    userChoices();
+  }
+  
+  else {
+    return;
+  }
 }
